06c6e446991d6668a3c490732592e16c0b12e11e
Caroline Lienard <carolinemrd@outlook.fr>
2022-06-23 12:32:35 +0200

    🔨 Adding scripts

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,70 +1,78 @@
-const myCart = localStorage.getItem("product")
-const myCartObj = JSON.parse(myCart)
+const myCart = JSON.parse(localStorage.getItem("product"))
 const cart = document.getElementById("cart__items")
 
-for ( let info of myCartObj ) {
+for ( let info of myCart ) {
     const productID = info.id
-    const productName = info.name
-    const productColor = info.color
-    const productImage = info.image
-    const productQuantity = info.quantity
-    console.log(info);
-        
-    const cartContainer = document.createElement("article")
-    cartContainer.classList.add("cart__item")
-    cart.appendChild(cartContainer)
+    
+    fetch(`http://localhost:3000/api/products/${productID}`)
+    .then(data => data.json())
+    .then(res => {
+        const productName = res.name
+        const productImage = res.imageUrl
+        const productPrice = res.price
 
-    const imageContainer = document.createElement("div")
-    imageContainer.classList.add("cart__item__img")
-    cartContainer.appendChild(imageContainer)
+        console.log(res);
 
-    const image = document.createElement("img")
-    image.src = productImage
-    imageContainer.appendChild(image)
+        const cartContainer = document.createElement("article")
+        cartContainer.classList.add("cart__item")
+        cart.appendChild(cartContainer)
 
-    const content = document.createElement("div")
-    content.classList.add("cart__item__content")
-    cartContainer.appendChild(content)
+        const imageContainer = document.createElement("div")
+        imageContainer.classList.add("cart__item__img")
+        cartContainer.appendChild(imageContainer)
 
-    const contentDescription = document.createElement("div")
-    contentDescription.classList.add("cart__item__content__description")
-    content.appendChild(contentDescription)
+        const image = document.createElement("img")
+        image.src = productImage
+        imageContainer.appendChild(image)
 
-    const name = document.createElement("h2")
-    contentDescription.appendChild(name)
-    name.innerHTML = info.name
+        const content = document.createElement("div")
+        content.classList.add("cart__item__content")
+        cartContainer.appendChild(content)
 
-    const color = document.createElement("p")
-    contentDescription.appendChild(color)
-    color.innerHTML = info.color
+        const contentDescription = document.createElement("div")
+        contentDescription.classList.add("cart__item__content__description")
+        content.appendChild(contentDescription)
 
-    const price = document.createElement("p")
-    contentDescription.appendChild(price)
-    price.innerHTML = info.price + "€"
+        const name = document.createElement("h2")
+        contentDescription.appendChild(name)
+        name.innerHTML = productName
 
-    const contentSettings = document.createElement("div")
-    contentSettings.classList.add("cart__item__content__settings")
-    content.appendChild(contentSettings)
+        const color = document.createElement("p")
+        contentDescription.appendChild(color)
+        color.innerHTML = info.color
 
-    const qty = document.createElement("p")
-    contentSettings.appendChild(qty)
-    qty.innerHTML = "Qté:"
+        const price = document.createElement("p")
+        contentDescription.appendChild(price)
+        price.innerHTML = productPrice + "€"
 
-    const qtyInput = document.createElement("input")
-    qtyInput.type = "number"
-    qtyInput.classList.add("itemQuantity")
-    qtyInput.value = info.quantity
-    qtyInput.max = 100
-    qtyInput.min = 1
-    qtyInput.name = "itemQuantity"
-    contentSettings.appendChild(qtyInput)
+        contentSettings = document.createElement("div")
+        contentSettings.classList.add("cart__item__content__settings")
+        content.appendChild(contentSettings)
 
-    const deteled = document.createElement("div")
-    deteled.classList.add("cart__item__content__settings__delete")
-    contentSettings.appendChild(deteled)
+        const contentSettingsQty = document.createElement("div")
+        contentSettingsQty.classList.add("cart__item__content__settings__quantity")
+        contentSettings.appendChild(contentSettingsQty)
 
-    const deteledButton = document.createElement("p")
-    deteledButton.classList.add("deleteItem")
-    deteledButton.innerHTML = "Supprimer"
-    deteled.appendChild(deteledButton)    
+        const qty = document.createElement("p")
+        contentSettingsQty.appendChild(qty)
+        qty.innerHTML = "Qté:"
+
+        const qtyInput = document.createElement("input")
+        qtyInput.type = "number"
+        qtyInput.classList.add("itemQuantity")
+        qtyInput.value = info.quantity
+        qtyInput.max = 100
+        qtyInput.min = 1
+        qtyInput.name = "itemQuantity"
+        contentSettingsQty.appendChild(qtyInput)
+
+        const deteled = document.createElement("div")
+        deteled.classList.add("cart__item__content__settings__delete")
+        contentSettings.appendChild(deteled)
+
+        const deteledButton = document.createElement("p")
+        deteledButton.classList.add("deleteItem")
+        deteledButton.innerHTML = "Supprimer"
+        deteled.appendChild(deteledButton)
+    }) 
 }
